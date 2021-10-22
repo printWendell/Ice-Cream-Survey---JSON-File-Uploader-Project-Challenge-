b686e1dbd6caaec4819259f58dbb437df46f153e
Wendell <superwendell46@gmail.com>
2021-10-22 11:08:54 -0400

    add sorted list function to return top 5 voted icecream from json file

--- app/__init__.py
+++ app/__init__.py
@@ -4,15 +4,19 @@ from flask import Flask, make_response, jsonify, json, request, redirect, url_fo
 app = Flask(__name__)
 app.config['UPLOAD_EXTENSIONS'] = ['.json']
 
+def returnSortedList(data):
+    # sort json data by votes
+    sortedList = sorted(data, key=lambda cream: cream['votes'], reverse=True)
+    icecream = sortedList[:5]
+    return(icecream)
+
 @app.route('/')
 def get_json_file():
     with open("app/static/data/total_count.json") as file:
         data = json.load(file)
-        # sort json data by votes
-        sortedList = sorted(data, key=lambda cream: cream['votes'], reverse=True)
 
         # send top five results 
-        res = make_response(jsonify(sortedList[:5]),200)
+        res = make_response(jsonify(returnSortedList(data)),200)
         return res
 
 @app.route('/add_votes')
@@ -31,17 +35,16 @@ def update_json_file():
                 # loop through local file and match flavor to new json file
                 with open("app/static/data/total_count.json", 'r+') as file:
                     data = json.load(file)
-                    
+
                     # find more efficient method of looping through files later
                     for oldvotes in data:
                         for updatedvotes in json_file:
                             if oldvotes['flavor'] == updatedvotes['flavor']:
                                 oldvotes['votes'] += updatedvotes['votes']
-                    sortedList = sorted(data, key=lambda cream: cream['votes'], reverse=True)
                     file.seek(0)
                     file.write(json.dumps(data))
                     file.truncate()
-                    return redirect(url_for('get_json_file', code=200, icecream=sortedList[:5]))
+                    return redirect(url_for('get_json_file', code=200, icecream=returnSortedList(data)))
             except:
                 message = {
                 'status': 500,
@@ -49,6 +52,7 @@ def update_json_file():
             }
             return redirect('update_json_file', message=message), 500
         else: 
+            # file is not json file
             message = {
                 'status': 400,
                 'message': 'Please upload valid JSON file'
@@ -56,6 +60,7 @@ def update_json_file():
             return redirect(url_for('update_json_file', message=message)), 400
 
     else:
+        # if request is a GET route
         return redirect(url_for(update_json_file))
 
 
