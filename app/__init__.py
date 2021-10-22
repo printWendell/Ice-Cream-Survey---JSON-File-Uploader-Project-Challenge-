7e5f4c272892e9b9a367e0f1ef342f27d895290a
Wendell <superwendell46@gmail.com>
2021-10-22 01:39:01 -0400

    create get_json_file function to read json file from static folder

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,17 @@
-from flask import Flask
+from flask import Flask, make_response, jsonify, json
 
 app = Flask(__name__)
 
 @app.route('/')
-def index():
-    return "Hello World"
+def get_json_file():
+    with open("app/static/data/total_count.json") as file:
+        data = json.load(file)
+        # sort json data by votes
+        sortedList = sorted(data, key=lambda cream: cream['votes'], reverse=True)
+
+        # send top five results 
+        res = make_response(jsonify(sortedList[:5]),200)
+        return res
 
 if __name__ == '__main__':
     app.run(debug=True)
